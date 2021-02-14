4ce08218a4af077c28d7d362af5a4720b9d29c87
hirossan4049 <haruto405329@gmail.com>
2021-02-14 18:12:20 +0900

    update

--- setup.py
+++ setup.py
@@ -1,2 +1,9 @@
 from setuptools import setup
-setup()
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
